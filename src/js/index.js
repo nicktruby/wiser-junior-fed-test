7940a58a22a80f1ffbfca8cd56c51688835c1e25
Nick Truby <nick@truby.io>
2021-01-06 16:56:04 +0000

    test submission

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("ready");
 });
+
+// Card state change
+
+const card = document.querySelector('.card');
+const cardToggle = card.querySelector('.card__toggle');
+
+cardToggle.addEventListener('click', () => card.classList.toggle('active'))
